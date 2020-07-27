2da346467580b88520532eb6a9fa9de0ffff66e7
dqn <suyarizzz@gmail.com>
2020-07-27 23:59:54 +0900

    test: update test

--- test/json.test.ts
+++ test/json.test.ts
@@ -3,12 +3,12 @@ import { parse } from '../src/json';
 describe('parse', () => {
   test('', () => {
     const act = parse(
-      '{"foo":"bar","baz":42,"qux":{"quux":["corge",{"grault":"garply"},[23,"waldo"]]}}',
+      '{"foo":"bar","baz":42,"qux":{"quux":["corge",{"grault":"garply"},[23,"waldo",true,false,],null]}}',
     );
     const exp = {
       foo: 'bar',
       baz: 42,
-      qux: { quux: ['corge', { grault: 'garply' }, [23, 'waldo']] },
+      qux: { quux: ['corge', { grault: 'garply' }, [23, 'waldo', true, false], null] },
     };
     expect(act).toEqual(exp);
   });
