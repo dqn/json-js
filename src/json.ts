037ad2425dd1812ed7e5bce98ed5f938cd088721
dqn <suyarizzz@gmail.com>
2020-07-27 23:59:04 +0900

    refactor: simplified parser

--- src/json.ts
+++ src/json.ts
@@ -1,140 +1,132 @@
-const SPACE_REGEXP = /\s/g;
-const NUMBER_REGEXP = /\d/;
-
 type JsonValue = null | string | number | boolean | { [key: string]: JsonValue } | JsonValue[];
 
-class Parser {
-  chars: string[] = [];
+export function parse(text: string) {
+  const chars = text.replace(/\s/g, '').split('');
 
-  constructor(text: string) {
-    this.chars = text.replace(SPACE_REGEXP, '').split('');
-  }
-
-  next(expect: string | RegExp) {
+  const next = (expect: string | RegExp): boolean => {
     if (typeof expect === 'object') {
-      return expect.test(this.chars[0]);
+      return expect.test(chars[0]);
     } else if (typeof expect === 'string') {
-      return this.chars[0] === expect;
+      return chars[0] === expect;
     }
-  }
+    throw TypeError(`unknown argument type: ${typeof expect}`);
+  };
 
-  shift(): string | undefined {
-    return this.chars.shift();
-  }
+  const shift = (): string | undefined => {
+    return chars.shift();
+  };
 
-  consume(expect: string) {
+  const consume = (expect: string) => {
     for (const c of expect) {
-      if (!this.next(c)) {
-        throw new Error(`expect "${c}", but got "${this.chars[0]}"`);
+      if (!next(c)) {
+        throw new Error(`expect "${c}", but got "${chars[0]}"`);
       }
-      this.shift();
+      shift();
     }
-  }
+  };
 
-  parseObject(): { [key: string]: JsonValue } {
-    this.consume('{');
+  const parseObject = (): { [key: string]: JsonValue } => {
+    consume('{');
 
     const obj: { [key: string]: JsonValue } = {};
 
-    while (!this.next('}')) {
-      const key = this.parseString();
-      this.consume(':');
-      obj[key] = this.parse();
+    while (!next('}')) {
+      const key = parseString();
+      consume(':');
+      obj[key] = parse();
 
-      if (this.next(',')) {
-        this.shift();
+      if (next(',')) {
+        shift();
       }
     }
 
-    this.consume('}');
+    consume('}');
 
     return obj;
-  }
+  };
 
-  parseArray(): JsonValue[] {
-    this.consume('[');
+  const parseArray = (): JsonValue[] => {
+    consume('[');
 
     const arr: JsonValue = [];
 
-    while (!this.next(']')) {
-      arr.push(this.parse());
+    while (!next(']')) {
+      arr.push(parse());
 
-      if (this.next(',')) {
-        this.shift();
+      if (next(',')) {
+        shift();
       }
     }
 
-    this.consume(']');
+    consume(']');
 
     return arr;
-  }
+  };
 
-  parseString(): string {
-    this.consume('"');
+  const parseString = (): string => {
+    consume('"');
 
     let str = '';
     let skipNext = false;
 
     // TODO: escape
-    while (!this.next('"')) {
-      str += this.shift();
+    while (!next('"')) {
+      str += shift();
 
-      if (this.next('\\')) {
-        str += this.shift();
+      if (next('\\')) {
+        str += shift();
       }
     }
 
-    this.consume('"');
+    consume('"');
 
     return str;
-  }
+  };
 
-  parseNumber(): number {
+  const parseNumber = (): number => {
     let str = '';
 
-    while (this.next(NUMBER_REGEXP)) {
-      str += this.shift();
+    while (next(/\d/)) {
+      str += shift();
     }
 
     return Number(str);
-  }
+  };
 
-  parseTrue(): true {
-    this.consume('true');
+  const parseTrue = (): true => {
+    consume('true');
     return true;
-  }
+  };
 
-  parseFalse(): false {
-    this.consume('false');
+  const parseFalse = (): false => {
+    consume('false');
     return false;
-  }
+  };
 
-  parseNull(): null {
-    this.consume('null');
+  const parseNull = (): null => {
+    consume('null');
     return null;
-  }
+  };
 
-  parse(): any {
-    if (this.next('{')) {
-      return this.parseObject();
-    } else if (this.next('[')) {
-      return this.parseArray();
-    } else if (this.next('"')) {
-      return this.parseString();
-    } else if (this.next(NUMBER_REGEXP)) {
-      return this.parseNumber();
-    } else if (this.next('t')) {
-      return this.parseTrue();
-    } else if (this.next('f')) {
-      return this.parseFalse();
-    } else if (this.next('n')) {
-      return this.parseNull();
+  const parse = (): any => {
+    if (next('{')) {
+      return parseObject();
+    } else if (next('[')) {
+      return parseArray();
+    } else if (next('"')) {
+      return parseString();
+    } else if (next(/\d/)) {
+      return parseNumber();
+    } else if (next('t')) {
+      return parseTrue();
+    } else if (next('f')) {
+      return parseFalse();
+    } else if (next('n')) {
+      return parseNull();
     } else {
       throw new Error(`failed to parse the JSON`);
     }
-  }
-}
+  };
 
-export function parse(text: string) {
-  return new Parser(text).parse();
+  return parse();
 }
