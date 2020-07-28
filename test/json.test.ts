5dea481e30078b305a92aaee3d12d8260402e013
dqn <suyarizzz@gmail.com>
2020-07-28 21:28:50 +0900

    test: update test

--- test/json.test.ts
+++ test/json.test.ts
@@ -1,7 +1,51 @@
 import { parse } from '../src/json';
 
 describe('parse', () => {
-  test('', () => {
+  test('null', () => {
+    expect(parse('null')).toEqual(null);
+  });
+
+  test('true', () => {
+    expect(parse('true')).toEqual(true);
+  });
+
+  test('false', () => {
+    expect(parse('false')).toEqual(false);
+  });
+
+  test('string', () => {
+    expect(parse('"xxx"')).toEqual('xxx');
+  });
+
+  test('number', () => {
+    expect(parse('42')).toEqual(42);
+  });
+
+  test('object', () => {
+    expect(parse('{"xxx": "yyy", "yyy": "zzz"}')).toEqual({ xxx: 'yyy', yyy: 'zzz' });
+  });
+
+  test('empty object', () => {
+    expect(parse('{}')).toEqual({});
+  });
+
+  test('nested object', () => {
+    expect(parse('{"xxx": {"yyy": {"zzz": null}}}')).toEqual({ xxx: { yyy: { zzz: null } } });
+  });
+
+  test('array', () => {
+    expect(parse('[42, "xxx", {}, null]')).toEqual([42, 'xxx', {}, null]);
+  });
+
+  test('empty array', () => {
+    expect(parse('[]')).toEqual([]);
+  });
+
+  test('nested array', () => {
+    expect(parse('[[[null]]]')).toEqual([[[null]]]);
+  });
+
+  test('complex', () => {
     const act = parse(
       '{"foo":"bar","baz":42,"qux":{"quux":["corge",{"grault":"garply"},[23,"waldo",true,false,],null]}}',
     );
@@ -11,5 +55,18 @@ describe('parse', () => {
       qux: { quux: ['corge', { grault: 'garply' }, [23, 'waldo', true, false], null] },
     };
     expect(act).toEqual(exp);
+  });
+
+  test('multiple line', () => {
+    const text = `
+{
+  "xxx": {
+    "yyy": {
+      "zzz": null
+    }
+  }
+}
+`;
+    expect(parse(text)).toEqual({ xxx: { yyy: { zzz: null } } });
   });
 });
