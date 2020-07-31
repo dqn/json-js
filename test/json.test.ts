c5101df4464a2afea18e87e12aeaa6f9d260dc6e
dqn <suyarizzz@gmail.com>
2020-08-01 06:41:01 +0900

    test: add stringify test cases

--- test/json.test.ts
+++ test/json.test.ts
@@ -1,4 +1,4 @@
-import { parse } from '../src/json';
+import { parse, stringify } from '../src/json';
 
 describe('parse', () => {
   test('null', () => {
@@ -79,5 +79,71 @@ describe('parse', () => {
 }
 `;
     expect(parse(val)).toEqual(JSON.parse(val));
+  });
+});
+
+describe('stringify', () => {
+  test('null', () => {
+    const val = null;
+    expect(stringify(val)).toEqual(JSON.stringify(val));
+  });
+
+  test('true', () => {
+    const val = true;
+    expect(stringify(val)).toEqual(JSON.stringify(val));
+  });
+
+  test('false', () => {
+    const val = false;
+    expect(stringify(val)).toEqual(JSON.stringify(val));
+  });
+
+  test('string', () => {
+    const val = 'xxx';
+    expect(stringify(val)).toEqual(JSON.stringify(val));
+  });
+
+  test('number', () => {
+    const val = 42;
+    expect(stringify(val)).toEqual(JSON.stringify(val));
+  });
+
+  test('object', () => {
+    const val = { xxx: 'yyy', yyy: 'zzz' };
+    expect(stringify(val)).toEqual(JSON.stringify(val));
+  });
+
+  test('empty object', () => {
+    const val = {};
+    expect(stringify(val)).toEqual(JSON.stringify(val));
+  });
+
+  test('nested object', () => {
+    const val = { xxx: { yyy: { zzz: null } } };
+    expect(stringify(val)).toEqual(JSON.stringify(val));
+  });
+
+  test('array', () => {
+    const val = [42, 'xxx', {}, null];
+    expect(stringify(val)).toEqual(JSON.stringify(val));
+  });
+
+  test('empty array', () => {
+    const val: undefined[] = [];
+    expect(stringify(val)).toEqual(JSON.stringify(val));
+  });
+
+  test('nested array', () => {
+    const val = [[[null]]];
+    expect(stringify(val)).toEqual(JSON.stringify(val));
+  });
+
+  test('complex', () => {
+    const val = {
+      foo: 'bar',
+      baz: 42,
+      qux: { quux: ['corge', { grault: 'garply' }, [23, 'waldo', true, false], null] },
+    };
+    expect(stringify(val)).toEqual(JSON.stringify(val));
   });
 });
